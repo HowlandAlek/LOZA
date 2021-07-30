d42a3f9a04e56c1454fcaf901c34396eb13098ff
RD13p <vicrodriguez9913@gmail.com>
2021-07-30 02:06:56 -0500

    Ajustes a disenio de consulta y producto

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const User = require("../model/user");
 const Product = require("../model/product");
+
 const app = express();
 
 app.get("/", async(req, res) => {
-    var productosDestacados = await Product.find({ Rarity: "High" });
+    var productosDestacados = await Product.find({ Rarity: "High" }).lean();
     // Obtiene los productos destacados  
-
     res.render("index", { productosDestacados });
+
 });
 
 app.get("/todasColecciones", async(req, res) => {
     // Obtiene todos los productos
-    var todosProductos = await Product.find();
-    res.render("consulta", { todosProductos });
+    var todosProductos = await Product.find().lean();
+    console.log(todosProductos);
+    res.render("consulta", { title: "Todas las colecciones", todosProductos });
 });
 
 module.exports = app;
