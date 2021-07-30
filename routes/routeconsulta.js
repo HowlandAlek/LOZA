484db9752a656075229b8dc12474893752a28ec2
RD13p <vicrodriguez9913@gmail.com>
2021-07-30 04:42:06 -0500

    A mimir

--- routes/routeconsulta.js
+++ routes/routeconsulta.js
@@ -2,27 +2,26 @@ const express = require("express");
 const Product = require("../model/product");
 const app = express();
 
-app.get("/:query", async (req, res) => {
+app.get("/:query", async(req, res) => {
     // Obtiene los productos que coincidan con la busqueda
     var query = req.params.query;
-
-    var productos = await Product.find({
+    var todosProductos = await Product.find({
         $or: [
             { Collection_ID: new RegExp(query) },
             { Name: new RegExp(query) },
             { Collection_Name: new RegExp(query) },
         ],
-    });
+    }).lean();
 
-    res.send(productos);
-    res.render("consulta", { productos });
+    //es.send(todosProductos);
+    res.render("consulta", { title: "Resultados para " + '"' + query + '"', todosProductos: todosProductos });
 });
 
-app.get("/", async (req, res) => {
+app.get("/", async(req, res) => {
     // Obtiene los productos que coincidan con la busqueda
     var query = req.params.query;
 
-    var productos = await Product.find();
+    var productos = await Product.find().lean();
     res.send(productos);
 });
 
