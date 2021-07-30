61afc096ecf521cde89606522b2697ea256e2e83
Renegal1238 <a01654359@itesm.mx>
2021-07-30 16:55:40 -0500

    wishlist

--- routes/routewishlist.js
+++ routes/routewishlist.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const Product = require("../model/product");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.render("wishlist");
+app.get("/", async (req, res) => {
+    var productosDestacados = await Product.find({ Rarity: "High" }).lean();
+    // Obtiene los productos destacados  
+    res.render("wishlist", { productosDestacados });
 });
 
 module.exports = app;
